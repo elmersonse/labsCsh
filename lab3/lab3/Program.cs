14b40ca01fb36bb0d20c8d73c6fb6c31a1f15d0d
elmersonse <pkititsa200@gmail.com>
2022-02-28 10:05:33 +0300

    lab3p1 done

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace lab3;
+class lab3
+{
+    delegate double Func(double x, double y);
 
-Console.WriteLine("Hello, World!");
+    static void Main()
+    {
+        Console.WriteLine("Выберите функцию (1 - f, 2 - g):");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите x:");
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите y:");
+        double y = double.Parse(Console.ReadLine());
+        double res;
+        switch (n)
+        {
+            case 1:
+                res = z(x, y, f);
+                break;
+            case 2:
+                res = z(x, y, g);
+                break;
+            default:
+                res = -1;
+                break;
+        }
+        Console.WriteLine("Результат выполнения метода = {0}", res);
+    }
+
+    static double f(double x, double y)
+    {
+        return 6 * x + y;
+    }
+
+    static double g(double x, double y)
+    {
+        return 5 * x - y + 5;
+    }
+
+    static double z(double x, double y, Func fn)
+    {
+        return fn(x, y) - 3 * fn(x, 2 * y);
+    }
+}
+
