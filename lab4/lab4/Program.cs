40d47afc40f9ea8c776959948e2d7f8b1f7625e0
elmersonse <pkititsa200@gmail.com>
2022-03-15 13:16:08 +0300

    lab4p1 done

--- lab4/lab4/Program.cs
+++ lab4/lab4/Program.cs
@@ -1,3 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.VisualBasic;
 
-Console.WriteLine("Hello, World!");
+namespace lab4
+{
+    class lab4
+    {
+        static void Main()
+        {
+            Console.WriteLine("1) Строки\n2) Текстовый файл\n3) Бинарный файл");
+            int c = Int32.Parse(Console.ReadLine());
+            switch (c)
+            {
+                case 1:
+                    Strings();
+                    break;
+            }
+        }
+
+        static void Strings()
+        {
+            Console.Clear();
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+            string newText = StringsDo(text);
+            if (newText.Equals("-1"))
+            {
+                Console.WriteLine("Пара слов отсутствует");
+            }
+            else
+            {
+                Console.WriteLine($"Полученная пара слов: {newText}");    
+            }
+        }
+
+        static string StringsDo(string text)
+        {
+            List<string> words = text.Split(' ').ToList();
+            List<string> reversedWords = new List<string>();
+            foreach (var word in words)
+            {
+                reversedWords.Add(new string(word.ToCharArray().Reverse().ToArray()));
+            }
+
+            foreach (var word in words)
+            {
+                if (reversedWords.Contains(word))
+                {
+                    return word + " " + new string(word.ToCharArray().Reverse().ToArray());
+                }
+                
+            }
+            return "-1";
+        }
+    }
+}
